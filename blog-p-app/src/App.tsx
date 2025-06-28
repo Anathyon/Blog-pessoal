67e8931f4329796609ffbc7c079584d1297963a5
Anathyon <anathyon@hotmail.com>
2025-06-27 21:51:21 -0300

    Barra navegação

--- blog-p-app/src/App.tsx
+++ blog-p-app/src/App.tsx
@@ -1,7 +1,12 @@
+import Barra_navegacao from "./components/barra_navegacao"
+
 export default function App() {
   return (
+    <section>
+        <Barra_navegacao/>
     <div className="text-center border-2">
       Blog pessoal React + Vite
     </div>
+    </section>
   )
 }
---
+++ blog-p-app/src/components/Barra_navegacao.tsx
@@ -0,0 +1,10 @@
+export default function barra_navegacao (){
+    return <>
+         <nav>
+             <button>inicio</button>
+             <button>contato</button>
+             <button>sobre</button>
+             <button>curriculo</button>
+         </nav>
+     </>
+}
